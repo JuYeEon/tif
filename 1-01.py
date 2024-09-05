886e27723079558ed72a2677547b8b3d98ac2d64
YeEon <dpdjs0831@gmail.com>
2024-09-05 17:04:56 +0900

    Update 1-01.py

--- 1-01.py
+++ 1-01.py
@@ -1,5 +1,6 @@
 num1 = []
 num2 = []
+result = 'False'
 for i in range(3):
   n = int(input('num1 : ))
   num1.append(n)
@@ -12,6 +13,4 @@ for i in range(0, 3):
     if num1[i] + num2[j] == target:
       result = 'True'
       break
-    else:
-      result = 'False'
 print(result)
